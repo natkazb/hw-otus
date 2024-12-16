159c0d0211eb930dea716ac65bc38aea4e5a6c32
n.zbiranik <n.zbiranik@cdek.ru>
2024-12-16 15:54:33 +0700

    hw 3

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,43 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+const LIMIT = 10
+
+type WordFrequency struct {
+	word  string
+	count int
+}
+
+func Top10(s string) []string {
+	words := strings.Fields(s)
+	frequency := make(map[string]int)
+	for _, v := range words {
+		frequency[v]++
+	}
+
+	pairs := make([]WordFrequency, 0, len(frequency))
+	for word, count := range frequency {
+		pairs = append(pairs, WordFrequency{word, count})
+	}
+
+	// Sort by frequency (desc) and alphabetically
+	sort.Slice(pairs, func(i, j int) bool {
+		if pairs[i].count == pairs[j].count {
+			return pairs[i].word < pairs[j].word
+		}
+		return pairs[i].count > pairs[j].count
+	})
+
+	// Take LIMIT or less
+	limit := min(LIMIT, len(pairs))
+	result := make([]string, limit)
+	for i := 0; i < limit; i++ {
+		result[i] = pairs[i].word
+	}
+
+	return result
 }
