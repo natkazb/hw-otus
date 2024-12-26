d91deea0664b7b6a81073d4b984d48ed593cc469
n.zbiranik <n.zbiranik@cdek.ru>
2024-12-26 23:00:16 +0700

    hw4 cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,8 +9,6 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -21,5 +19,38 @@ func NewCache(capacity int) Cache {
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
+	}
+}
+
+func (l *lruCache) Set(key Key, value interface{}) bool {
+	item, exists := l.items[key]
+	if exists {
+		item.Value = value
+		return true
+	} else {
+		newL := l.queue.PushBack(value)
+		l.items[key] = newL
+		return false
+	}
+}
+
+func (l *lruCache) Get(key Key) (interface{}, bool) {
+	item, exists := l.items[key]
+	if !exists {
+		return nil, false
+	}
+	return item.Value, true
+}
+
+func (l *lruCache) Clear() {
+	l.capacity = 0
+	l.items = make(map[Key]*ListItem)
+	if l.queue.Len() > 0 {
+		item := l.queue.Front()
+		for item != nil {
+			next := item.Next
+			l.queue.Remove(item)
+			item = next
+		}
 	}
 }
