afecd0994bb23f5e9b9e847f753b54858b04078e
n.zbiranik <n.zbiranik@cdek.ru>
2025-01-11 21:06:19 +0700

    hw5

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,6 +10,51 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
-	return nil
+	countOfTasks := 0
+	countOfErrors := 0
+
+	chEndTask := make(chan struct{}, n)
+	chAddError := make(chan struct{}, n)
+
+	lenTasks := len(tasks)
+	finish := lenTasks
+	if n < finish {
+		finish = n
+	}
+
+	i := 0
+	for ; i < finish; i++ {
+		countOfTasks++
+		go func(task Task) {
+			defer func() { chEndTask <- struct{}{} }()
+			if err := task(); err != nil {
+				chAddError <- struct{}{}
+			}
+		}(tasks[i])
+	}
+
+	for {
+		select {
+		case <-chAddError:
+			countOfErrors++
+			if countOfErrors >= m {
+				return ErrErrorsLimitExceeded
+			}
+		case <-chEndTask:
+			countOfTasks--
+			if i >= lenTasks {
+				return nil
+			}
+			if countOfTasks < n {
+				countOfTasks++
+				go func() {
+					defer func() { i++; chEndTask <- struct{}{} }()
+					if err := tasks[i](); err != nil {
+						chAddError <- struct{}{}
+					}
+				}()
+			}
+		}
+	}
+	// return nil
 }
