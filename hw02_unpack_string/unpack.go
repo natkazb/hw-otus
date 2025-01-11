c58a96371594e8fa19b202e84ff617d4194b5c26
n.zbiranik <n.zbiranik@cdek.ru>
2025-01-11 17:56:18 +0700

    asterisk

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -13,6 +13,7 @@ func Unpack(s string) (string, error) {
 	var result strings.Builder
 	var prevStr string
 	var prevRunes []rune
+	isBeginShield := false
 
 	gr := uniseg.NewGraphemes(s)
 	for gr.Next() {
@@ -20,7 +21,7 @@ func Unpack(s string) (string, error) {
 		currentRunes := gr.Runes()
 
 		// Handle numbers
-		if isNumber(currentRunes) {
+		if isNumber(currentRunes) && !isBeginShield {
 			if prevStr == "" || isNumber(prevRunes) {
 				return "", ErrInvalidString
 			}
@@ -30,12 +31,23 @@ func Unpack(s string) (string, error) {
 				result.WriteString(strings.Repeat(prevStr, count-1))
 			}
 		} else {
-			// Check for zero
+			// Check for next
 			_, to := gr.Positions()
 			gr2 := uniseg.NewGraphemes(s[to:])
 			okNext := gr2.Next()
-			if !okNext || okNext && !isZero(gr2.Runes()) {
+			if isShield(currentRunes) { // \
+				isBeginShield = true
+				if isShield(prevRunes) {
+					result.WriteString(currentStr)
+				}
+				if okNext && !isNumber(gr2.Runes()) && !isShield(gr2.Runes()) {
+					return "", ErrInvalidString
+				}
+			} else if !okNext || okNext && !isZero(gr2.Runes()) { // zero
 				result.WriteString(currentStr)
+				if isBeginShield && !isNumber(gr2.Runes()) {
+					isBeginShield = false
+				}
 			}
 		}
 		prevStr = currentStr
@@ -51,4 +63,8 @@ func isNumber(r []rune) bool {
 
 func isZero(r []rune) bool {
 	return len(r) == 1 && r[0] == 48
+}
+
+func isShield(r []rune) bool {
+	return len(r) == 1 && r[0] == 92
 }
